8efd4f0d12f84b70acc1ab49d62598fe36ca307d
dansmog <juwonanthony@gmail.com>
2017-08-06 20:47:33 +0100

    add book reducer and connect it with rootReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: "Javascript: The definitive Guide" },
+        { title: "The eloquent Javascript" },
+        { title: "Tongues above the upper room" },
+        { title: "Public speaking and influencing people" },
+    ]
+}
